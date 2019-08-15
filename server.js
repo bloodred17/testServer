887add32b0300a6ef4886748a75c2630db30e65c
Ankut Dutta <ankur.611@gmail.com>
2019-08-16 04:17:53 +0530

    views

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require ('express');
+const hbs = require('hbs');
 var app = express();
 var port = process.env.PORT || 3000;
+
+//Setting view engine for express
+app.set('view engine', 'hbs');
+
 app.get('/', (req, res) => {
-    res.send('Hello Node!');
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
-app.get('/on', (req, res) => {
-    res.send('1');
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.listen(port, function() {
